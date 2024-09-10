345fb971cb560d91212a76b50b977f87fd6baa2a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-09-11 05:05:29 +0530

    added open ai key

---
+++ server/src/config/openai.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(
+    openai_api_key=openai_api_key,
+    model_name="gpt-4o-mini",
+    temperature=0.0,
+    max_tokens=None,
+    max_retries=3,
+)
