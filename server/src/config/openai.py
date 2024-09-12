932b131eca9f53913f67d47f03ececd64883467e
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-09-12 08:08:27 +0530

    changed currrent model for experimentation

--- server/src/config/openai.py
+++ server/src/config/openai.py
@@ -9,7 +9,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(
     openai_api_key=openai_api_key,
-    model_name="gpt-4o",
+    model_name="gpt-4o-mini",
     temperature=0.0,
     max_tokens=None,
     max_retries=3,
