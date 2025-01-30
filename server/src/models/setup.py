09290659942bdad70a433f48bad5db5d89a13fae
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-30 10:23:51 +0530

    refactor: switch database configuration to SQLite for local development

--- server/src/models/setup.py
+++ server/src/models/setup.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://username:password@localhost/dbname"
+DATABASE_URL = "sqlite:///./storage.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
