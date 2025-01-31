6c53e68716b08df3ba8d7a044c0cf0db6c209add
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-31 10:05:28 +0530

    feat: add comprehensive code generation and display components for LeetCode problem solutions

--- client/src/components/leetcode/Leetcode.tsx
+++ client/src/components/leetcode/Leetcode.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import Problems from "./Problems";
+import React, { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
+import ProblemDetail from "./questions/ProblemDetail";
+import LeetCodeSearch from "./search/LeetCodeSearch";
+import { Button } from "@/components/ui/button";
+import Problems from "./table/Problems";
+import useProblems from "@/store/leetcode/hook";
+import { useLocation } from "react-router-dom";
 
 const Leetcode: React.FC = () => {
+  const { fetchInfo } = useProblems();
+  const location = useLocation();
+
+  const problemName = useMemo(
+    () => location.pathname.split("/").pop(),
+    [location.pathname]
+  );
+  const [showProblems, setShowProblems] = useState<boolean>(!problemName);
+
+  useEffect(() => {
+    setShowProblems(!problemName);
+  }, [problemName]);
+
+  useEffect(() => {
+    fetchInfo();
+  }, [fetchInfo]);
+
+  console.log(problemName);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -10,7 +34,14 @@ const Leetcode: React.FC = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
     >
-      <Problems />
+      <div className="flex items-center justify-center my-4">
+        <Button onClick={() => setShowProblems(true)}>Problems</Button>
+        <LeetCodeSearch />
+      </div>
+      {!showProblems && problemName && (
+        <ProblemDetail problemName={problemName} />
+      )}
+      {showProblems && <Problems />}
     </motion.div>
   );
 };
