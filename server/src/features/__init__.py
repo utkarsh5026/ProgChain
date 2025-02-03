00b56e740654c19d5b2fbef3e643809159c2a949
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-02-04 05:13:01 +0530

    refactor: reorganize feature handlers and remove deprecated explore and problems modules

---
+++ server/src/features/__init__.py
@@ -0,0 +1,8 @@
+from .topics import topics_router
+from .interview import interview_router
+from .leetcode import leetcode_router
+from .explore import explore_router
+
+
+__all__ = ["topics_router", "interview_router",
+           "leetcode_router", "explore_router"]
--- server/src/server.py
+++ server/src/server.py
@@ -1,15 +1,12 @@
 from config.fast import app
-from config.openai import llm
-from handlers.topics import router as topics_router
-from handlers.explore import router as explore_router
-from handlers.interview import router as interview_router
+from features import topics_router, interview_router, leetcode_router, explore_router
 from handlers.quiz import router as quiz_router
 from handlers.flow import router as flow_router
-from handlers.problems import router as problems_router
+
 
 app.include_router(topics_router)
-app.include_router(explore_router)
 app.include_router(interview_router)
+app.include_router(leetcode_router)
+app.include_router(explore_router)
 app.include_router(quiz_router)
 app.include_router(flow_router)
-app.include_router(problems_router)
