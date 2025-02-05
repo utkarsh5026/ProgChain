e177239412cd8d2a5df31a08e1160a14a380f028
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-02-05 11:17:44 +0530

    feat: add @radix-ui/react-accordion dependency and implement accordion animations in Tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -52,7 +52,30 @@ module.exports = {
         },
       },
       fontFamily: {
-        sans: ['"Cascadia Code"', "monospace"],
+        mono: ["Cascadia Code", "monospace"],
+        sans: ["Cascadia Code", "monospace"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
