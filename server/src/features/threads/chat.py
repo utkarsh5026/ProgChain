8e28e49f22371ff6a86d8910d6961159c352aa16
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-02-20 19:53:48 +0530

    feat: enhance ThreadIDChat class with detailed docstrings for methods and initialization

--- server/src/features/threads/chat.py
+++ server/src/features/threads/chat.py
@@ -13,8 +13,27 @@ class ThreadIDChatError(Exception):
 
 
 class ThreadIDChat(BaseChatSystem):
+    """
+    ThreadIDChat is a class that manages chat interactions for a specific thread content.
+
+    It allows for the creation of chat instances associated with a thread content ID and facilitates
+    streaming chat responses based on user questions.
+    """
+
     @classmethod
     async def create(cls, thread_content_id: str) -> "ThreadIDChat":
+        """
+        Asynchronously creates a ThreadIDChat instance for the specified thread content ID.
+
+        Args:
+            thread_content_id (str): The unique identifier for the thread content.
+
+        Returns:
+            ThreadIDChat: An instance of ThreadIDChat initialized with the thread content.
+
+        Raises:
+            ThreadIDChatError: If the thread content with the specified ID is not found.
+        """
         try:
             thread_content = await models.ThreadContent.get_by_public_id(
                 thread_content_id)
@@ -33,11 +52,29 @@ class ThreadIDChat(BaseChatSystem):
                  vector_db: Optional[VectorDB] = None,
                  config: Optional[ChatConfig] = None,
                  initial_context: str = "") -> None:
+        """
+        Initializes a ThreadIDChat instance.
 
+        Args:
+            thread_content_id (str): The unique identifier for the thread content.
+            prompt (Optional[ChatPromptTemplate]): The prompt template for generating responses.
+            vector_db (Optional[VectorDB]): The vector database for chat data.
+            config (Optional[ChatConfig]): Configuration options for the chat system.
+            initial_context (str): The initial context associated with the thread.
+        """
         super().__init__(prompt, vector_db, config, initial_context)
         self.thread_content_id = thread_content_id
 
     async def stream_chat(self, options: ChatGenerateOptions) -> AsyncGenerator[dict, None]:
+        """
+        Asynchronously streams chat responses based on the provided options.
+
+        Args:
+            options (ChatGenerateOptions): Options for generating chat responses, including the user's question.
+
+        Yields:
+            dict: A dictionary containing the content ID, generated content, and associated metadata.
+        """
         contents = []
         try:
             async for chunk, metadata in self.generate_response(options):
